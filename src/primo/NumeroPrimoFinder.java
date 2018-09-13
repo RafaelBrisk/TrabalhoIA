2662e1dfac9e3d4be20b20d9b60f71270ce6ebac
RBRISKIEVICZ <RBRISKIEVICZ@WS-LAB08_116.univel.univel.br>
2018-09-13 19:55:22 -0300

    Correção

--- src/primo/NumeroPrimoFinder.java
+++ src/primo/NumeroPrimoFinder.java
@@ -9,7 +9,13 @@ public class NumeroPrimoFinder {
 	
 		boolean retorno = false;
 		
-		for (int i = 3; i < numero; i += 2) {
+		int metade = numero / 2;
+		
+		if (metade % 2 == 0) {
+			metade--;
+		}
+		
+		for (int i = 3; i < metade; i += 2) {
 			retorno = (numero % i == 0);
 			if (retorno) {
 				break;
@@ -24,7 +30,7 @@ public class NumeroPrimoFinder {
 		cronometro.iniciar();
 		
 		long primo = 0;
-		long numeroStop = 999999;
+		long numeroStop = 9999999;
 		for (int numero = 33; numero < numeroStop; numero += 2) {
 			if (EhPrimo(numero)) {
 				primo = numero;
